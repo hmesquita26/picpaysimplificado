4e08ff5a8d1353507389c9fa8c416fce2975a029
Heytor Mesquita <dev.hmesquita26@gmail.com>
2025-04-17 17:17:17 -0300

    feat: create flow with service and controller and changes in user entity

--- picpaysimplificado/src/main/java/com/picpay/picpaysimplificado/domain/user/User.java
+++ picpaysimplificado/src/main/java/com/picpay/picpaysimplificado/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.picpay.picpaysimplificado.domain.user;
 
+import com.picpay.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +11,7 @@ import java.math.BigDecimal;
 @Entity(name="users")
 @Table(name="users")
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
@@ -35,4 +34,14 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.document = data.document();
+        this.email = data.email();
+        this.password = data.password();
+    }
 }
